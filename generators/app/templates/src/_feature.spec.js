8526d37890a9b8cb0ca84d271e1d7e922147dc3a
ekisner <eric@lifesite.co>
2016-10-28 09:35:22 -0400

    -Refactor spec files to include component and effect tests (if it's a smart component). -Remove "SharedModule" since this can be a generic boilerplate helper. -Added "async" to the beforeEach to handle templateUrls.

--- generators/app/templates/src/_feature.spec.js
+++ generators/app/templates/src/_feature.spec.js
@@ -1,38 +1,55 @@
 <% if(isSmart) { %>
-import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject, async } from '@angular/core/testing';
 import { StoreModule } from '@ngrx/store';
+import { EffectsTestingModule, EffectsRunner } from '@ngrx/effects/testing';
 
 import { <%= inCamelCase %>Reducer, I<%= className %>State } from './<%= featureName %>.reducer';
 import { <%= className %> } from './<%= featureName %>.component';
+import { <%= className %>Effects } from './<%= featureName %>.effects';
 import * as <%= inCamelCase %>Actions from './<%= featureName %>.actions';
 
 describe('<%= className %>Component', () => {
-    const spy = jasmine.createSpy('<%= inCamelCase %>').and.callFake(function (state, action) {
-        return <%= inCamelCase %>Reducer(state, action);
-    });
-    let componentInstance: <%= className %>;
-    let fixture;
-
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            declarations: [<%= className %>],
-            imports: [
-                SharedModule,
-                StoreModule.provideStore({<%= inCamelCase %>: spy})
-            ],
-            schemas: [NO_ERRORS_SCHEMA]
+    describe('component tests', () => {
+        const spy = jasmine.createSpy('<%= inCamelCase %>').and.callFake(function (state, action) {
+            return <%= inCamelCase %>Reducer(state, action);
         });
+        let componentInstance: <%= className %>;
+        let fixture;
 
-        TestBed.compileComponents();
+        beforeEach(async(() => {
+            TestBed.configureTestingModule({
+                declarations: [<%= className %>],
+                imports: [
+                    StoreModule.provideStore({<%= inCamelCase %>: spy})
+                ]
+            }).compileComponents();
+        }));
 
-        fixture = TestBed.createComponent(<%= className %>);
-        fixture.detectChanges();
-        componentInstance = (<<%= className %>>fixture.componentInstance);
+        beforeEach(() => {
+            fixture = TestBed.createComponent(<%= className %>);
+            fixture.detectChanges();
+            componentInstance = (<<%= className %>>fixture.componentInstance);
+        });
+    });
+
+    describe('effects tests', () => {
+        let runner: EffectsRunner;
+        let effects: <%= className %>Effects;
+
+        beforeEach(() => TestBed.configureTestingModule({
+            imports: [EffectsTestingModule],
+            providers: [<%= className %>Effects]
+        }));
+
+        beforeEach(inject([
+            EffectsRunner, <%= className %>Effects
+        ], (_runner, _effects) => {
+            runner = _runner;
+            effects = _effects;
+        }));
     });
 });
 <% } else { %>
-import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 
 import { <%= className %> } from './<%= featureName %>.component';
@@ -43,8 +60,7 @@ describe('<%= className %>Component', () => {
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            declarations: [<%= className %>],
-            schemas: [NO_ERRORS_SCHEMA]
+            declarations: [<%= className %>]
         });
 
         TestBed.compileComponents();
